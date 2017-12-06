60df925e6f695deb0eb9e7cf58d2098e5267f736
danielmoralesp <daniel_moralesp@hotmail.com>
2017-12-06 18:12:59 -0500

    AddHashIdAndTimeZoneToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206230508) do
+ActiveRecord::Schema.define(version: 20171206231150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20171206230508) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "hash_id"
+    t.string "time_zone"
+    t.index ["hash_id"], name: "index_users_on_hash_id"
   end
 
 end
