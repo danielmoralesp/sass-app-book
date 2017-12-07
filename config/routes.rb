942636389ebd29b13769bf952f0b52c5cb34ce96
danielmoralesp <daniel_moralesp@hotmail.com>
2017-12-06 20:45:16 -0500

    Account controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :accounts
+
   get 'activity/mine'
 
   get 'activity/feed'
 
   root to: 'activity#mine'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
